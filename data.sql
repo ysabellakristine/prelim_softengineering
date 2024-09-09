c5c3c85ea10523cef9698cf81216d94b6822b8bf
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-09-09 22:31:23 +0800

    Update data.sql

--- data.sql
+++ data.sql
@@ -4,3 +4,9 @@ insert into Users (User_id, Username, Password, Email, Address) values (358907, 
 insert into Users (User_id, Username, Password, Email, Address) values (185191, 'Arnaldo', 'sL8{=>zGr=', 'abonhill3@artisteer.com', '3 Chive Crossing');
 insert into Users (User_id, Username, Password, Email, Address) values (600960, 'Theresina', 'rF3*_Qn0cJmF', 'tguild4@marketwatch.com', '7 Lunder Hill');
 
+insert into Books (Book_id, Title, Author, Genre, ISBN, Book_Status, Borrow_Price, Stock_Available) values (1927408439, 'donec diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante ipsum primis in', 'Amos Warbys', 'Fiction', '422266226-7', 'Unavailable', 11.59, 1);
+insert into Books (Book_id, Title, Author, Genre, ISBN, Book_Status, Borrow_Price, Stock_Available) values (1442349208, 'ut dolor morbi vel lectus in quam', 'Adara McKearnen', 'Non-Fiction', '534704586-8', 'Unavailable', 13.12, 0);
+insert into Books (Book_id, Title, Author, Genre, ISBN, Book_Status, Borrow_Price, Stock_Available) values (1522508797, 'fermentum justo nec condimentum neque sapien placerat ante nulla justo', 'Marcille Reeds', 'Fantasy', '043267713-5', 'Unavailable', 35.67, 4);
+insert into Books (Book_id, Title, Author, Genre, ISBN, Book_Status, Borrow_Price, Stock_Available) values (291617167, 'eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula', 'Danni Storm', 'Horror', '302677277-1', 'Available', 19.57, 3);
+insert into Books (Book_id, Title, Author, Genre, ISBN, Book_Status, Borrow_Price, Stock_Available) values (964036726, 'vel sem sed sagittis nam congue risus semper porta volutpat quam pede', 'Link Quartly', 'Mystery', '502117000-7', 'Unavailable', 19.25, 2);
+
