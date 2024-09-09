b99be5052bf0a6d492fefa6cc5229ec9551e5b01
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-09-09 22:27:47 +0800

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,6 @@
+insert into Users (User_id, Username, Password, Email, Address) values (219502, 'Brana', 'xP6~*~rutl9b+e~_', 'bcassely0@netscape.com', '64 Independence Lane');
+insert into Users (User_id, Username, Password, Email, Address) values (648401, 'Dulcie', 'yJ6{S(qT', 'dfarans1@businessinsider.com', '5 Lyons Lane');
+insert into Users (User_id, Username, Password, Email, Address) values (358907, 'Meyer', 'vY3>QyW/Py1', 'mcorney2@google.pl', '544 Ruskin Trail');
+insert into Users (User_id, Username, Password, Email, Address) values (185191, 'Arnaldo', 'sL8{=>zGr=', 'abonhill3@artisteer.com', '3 Chive Crossing');
+insert into Users (User_id, Username, Password, Email, Address) values (600960, 'Theresina', 'rF3*_Qn0cJmF', 'tguild4@marketwatch.com', '7 Lunder Hill');
+
